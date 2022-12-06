0a82700e070c1331747cc378e62caf15ae436a4c
miincho <112492904+miincho@users.noreply.github.com>
2022-12-05 20:14:28 -0500

    homepage created

---
+++ entries/entry11/script.js
@@ -0,0 +1,11 @@
+var popupWindow = null;
+function positionedPopup(url,winName,w,h,t,l,scroll){
+settings =
+'height='+h+',width='+w+',top='+t+',left='+l+',scrollbars='+scroll+',resizable'
+popupWindow = window.open(url,winName,settings)
+}
+
+
+$(document).ready(function() {$(".entry").draggable(); })
+
+Splitting();
