e5ae2e3892eb2b2a4b53fb540ab2dec22e72a285
Minkyung Cho <112492904+miincho@users.noreply.github.com>
2024-03-24 03:24:53 -0400

    api stuff

--- jshw/archive/main.js
+++ jshw/archive/main.js
@@ -1,32 +1,43 @@
-const url = `https://api.artic.edu/api/v1/artworks?page=${pagenum}&limit=100`; 
-var pagenum = 50;
+function changepages(pageNumber) {
+    const url = `https://api.artic.edu/api/v1/artworks?page=${pageNumber}&limit=100`;
 
-fetch(url)
-    .then(response => response.json())
-    .then((resp) => {
-        const imgContainer = document.getElementById("images");
-        resp.data.forEach(artwork =>{
-            const art = document.createElement("img");
-            const BaseUrl = resp.config.iiif_url;
-            const imageId = artwork.image_id;
-            const iiifUrl = `${BaseUrl}/${imageId}/full/,150/0/default.jpg`;
-            art.src = iiifUrl;     
-            imgContainer.appendChild(art);
-        });
+    fetch(url)
+        .then(response => response.json())
+        .then((resp) => {
+            const imgContainer = document.getElementById("images");
+            imgContainer.innerHTML = "";
 
-        // art.addEvent
-    })
-    .catch(error => console.error("error", error));
+            resp.data.forEach(artwork =>{
+                const art = document.createElement("img");
+                const BaseUrl = resp.config.iiif_url;
+                const imageId = artwork.image_id;
+                const iiifUrl = `${BaseUrl}/${imageId}/full/,150/0/default.jpg`;
+                art.src = iiifUrl;     
+                imgContainer.appendChild(art);
+            });
 
+            art.addEventListener('click', toggleBlur);
+    
+            function toggleBlur() {
+              this.classList.toggle('blur');
+            }
+        })
+    // .then(resp => console.log(resp))
+        .catch(error => console.error("error", error));
+}
 
-    function randomizePage(){
-        const pagenum = Math.floor(Math.random() * 9398);
-        // console.log(pagenum);
-    }
+function randomizePage() {
+    const newnum = Math.floor(Math.random() * 9398);
+    changepages(newnum);
+    // console.log(newnum);
+}
 
-    const btn = document.getElementById("rand");
-    btn.addEventListener("click", randomizePage);
+const btn = document.getElementById("rand");
+btn.addEventListener("click", randomizePage);
 
+// default page 50
+const defaultnum = 50;
+changepages(defaultnum);
 
 
     const peopleFilter = document.getElementById("people");
@@ -36,7 +47,7 @@ fetch(url)
     const waterFilter = document.getElementById("water");
     const noFilter = document.getElementById("everything");
 
-        // .then(resp => console.log(resp))
+
 
 
 /*image padding*/
