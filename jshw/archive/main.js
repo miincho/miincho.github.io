be8639fe311bb0bd9cf3ebc09528243f897a0a3e
Minkyung Cho <112492904+miincho@users.noreply.github.com>
2024-03-28 12:28:08 -0400

    im tired

--- jshw/archive/main.js
+++ jshw/archive/main.js
@@ -1,59 +1,267 @@
 function changepages(pageNumber) {
+    //api limits to 100 images per request; setting page number as a variable to add a randomize image feature later
     const url = `https://api.artic.edu/api/v1/artworks?page=${pageNumber}&limit=100`;
 
     fetch(url)
         .then(response => response.json())
+        // .then(resp => console.log(resp))
         .then((resp) => {
+            console.log(resp);
             const imgContainer = document.getElementById("images");
-            imgContainer.innerHTML = "";
+            imgContainer.innerHTML = ""; //remove previous images when page is randomized
 
             resp.data.forEach(artwork =>{
+                //creating image element
                 const art = document.createElement("img");
-                const BaseUrl = resp.config.iiif_url;
+
+                //creating iiif URL for each image
+                const baseUrl = resp.config.iiif_url;
                 const imageId = artwork.image_id;
-                const iiifUrl = `${BaseUrl}/${imageId}/full/,150/0/default.jpg`;
+                const iiifUrl = `${baseUrl}/${imageId}/full/,150/0/default.jpg`;
+
+                //getting info for each artwork;
+                const artistname = artwork.artist_display;
+                const artdate = artwork.title;
+                const artmedium = artwork.medium_display;
+                const artlocation = artwork.place_of_origin;
+
+
+                // Function to remove the existing popup div when another img is clicked
+                function removePopup() {
+                    const onscreen = document.querySelector('.popup-div');
+                    if (onscreen) {
+                        document.body.removeChild(onscreen);
+                    }
+                }
+
+                //dispalying info in a popup when each img is clicked
+                function infoPopup() {
+                    //calling functioln to clear previous div
+                    removePopup()
+
+                    //creating elements and linking the css to them
+                    const popupdiv = document.createElement('div');
+                    popupdiv.classList.add('popup-div');
+                
+                    const textDiv = document.createElement('div');
+                    textDiv.classList.add('popup-text-div');
+                
+                    const exitbtn = document.createElement('div');
+                    exitbtn.classList.add('exit-btn');
+                    exitbtn.textContent = 'X';
+                
+                    popupdiv.innerHTML = `
+                        <div class="popup-text-div">
+                        <span class="bold">Artist:</span> ${artistname}<br><br>
+                        <span class="bold">Date:</span> ${artdate}<br><br>
+                        <span class="bold">Medium:</span> ${artmedium}<br><br>
+                        <span class="bold">Location:</span> ${artlocation}
+                        </div>
+                    `;
+
+                    //duplicate image so it stays in the container
+                    const clonedArt = art.cloneNode(true);
+                    clonedArt.classList.add('cloned-art');
+
+
+                    //appending all elements to the div
+                    popupdiv.appendChild(clonedArt);
+                    popupdiv.appendChild(textDiv);
+                    popupdiv.appendChild(exitbtn);
+                
+                    //remove the popup div when the exit button is clicked
+                    exitbtn.addEventListener('click', function() {
+                        document.body.removeChild(popupdiv);
+                    });
+                
+                    //append div to the body
+                    document.body.appendChild(popupdiv);
+                }
+                art.addEventListener('click', infoPopup);
+
+
+
+                //appending  image element to image container
                 art.src = iiifUrl;     
                 imgContainer.appendChild(art);
+
+                // art.addEventListener('click', toggleBlur);
+                // function toggleBlur() {
+                // this.classList.toggle('blur');
+                // }
+
+
+
             });
-
-
         })
-    // .then(resp => console.log(resp))
         .catch(error => console.error("error", error));
 }
 
+//select a random page number from 1 through 9398
 function randomizePage() {
-    const newnum = Math.floor(Math.random() * 9398);
+    const newnum = Math.floor(Math.random() * 500);
     changepages(newnum);
-    // console.log(newnum);
+    console.log(newnum);
 }
 
 const btn = document.getElementById("rand");
 btn.addEventListener("click", randomizePage);
 
-// default page 50
-const defaultnum = 50;
+// set default page as 50
+const defaultnum = 500;
 changepages(defaultnum);
 
 
-    const peopleFilter = document.getElementById("people");
-    const animalFilter = document.getElementById("animals");
-    const natureFilter = document.getElementById("nature");
-    const foodFilter = document.getElementById("food");
-    const waterFilter = document.getElementById("water");
-    const noFilter = document.getElementById("everything");
+let blur = document.getElementById("blur");
+let size = document.getElementById("size");
+let contrast = document.getElementById("contrast");
+let invert = document.getElementById("invert");
+let saturation = document.getElementById("saturation");
 
-    // art.addEventListener('click', toggleBlur);
-    
-    // function toggleBlur() {
-    //   this.classList.toggle('blur');
-    // }
+const imgContainer = document.getElementById("images");
+
+//setting image values in an array to prevent overriding
+var filters = {
+    blur: 0,
+    size: 150,
+    contrast: 100,
+    invert: 0,
+    saturation: 100
+};
+
+function applyFilters() {
+    let images = imgContainer.querySelectorAll('img');
+    images.forEach(function(img) {
+        img.style.filter =  "blur(" + filters.blur + "px) " +
+                            "contrast(" + filters.contrast + "%) " +
+                            "invert(" + filters.invert + "%) " +
+                            "saturate(" + filters.saturation + "%)";
+        img.style.height = filters.size + "px";
+    });
+}
+
+function changeBlur() {
+    filters.blur = blur.value;
+    applyFilters();
+}
+
+function changeSize() {
+    filters.size = size.value;
+    applyFilters();
+}
+
+function changeContrast() {
+    filters.contrast = contrast.value;
+    applyFilters();
+}
+
+function changeInvert() {
+    filters.invert = invert.value;
+    applyFilters();
+}
+
+function changeSaturation() {
+    filters.saturation = saturation.value;
+    applyFilters();
+}
+
+//reset value button
+let reset = document.getElementById('resetbtn');
+
+reset.addEventListener('click', function(){
+    blur.value = 0;
+    size.value = 150;
+    contrast.value = 100;
+    invert.value = 0;
+    saturation.value = 150;
+
+    filters.blur = blur.value;
+    filters.size = size.value;
+    filters.contrast = contrast.value;
+    filters.invert = invert.value;
+    filters.saturation = saturation.value;
+
+    let images = imgContainer.querySelectorAll('img');
+    images.forEach(function(img) {
+        img.style.filter =  "blur(" + filters.blur + "px) " +
+                            "contrast(" + filters.contrast + "%) " +
+                            "invert(" + filters.invert + "%) " +
+                            "saturate(" + filters.saturation + "%)";
+        img.style.height = filters.size + "px";
+    });
+});
 
 
-/*image padding*/
-// const topbar = document.getElementsByClassName("topbar")[0];
-// function topPadding(){
-//     const width = topbar.offsetHeight;
-//     imgContainer.style.marginTop = `${width}px`;
+/*gallery page*/
+let gallerybtn = document.getElementById('gallerybtn');
+let galleryimgs = document.getElementById('galleryimgs');
+
+//remove images and show gallery
+function showgallery(){
+    if(imgContainer.style.display === 'block'){
+        imgContainer.style.display = 'none';
+        galleryimgs.style.display = 'block';
+    }
+    else{
+        imgContainer.style.display = 'block';
+        galleryimgs.style.display = 'none';
+    }
+}
+
+gallerybtn.addEventListener('click', showgallery);
+
+
+
+
+
+
+// function changeBlur(){
+//     filters.blur = blur.value;
+//     let images = imgContainer.querySelectorAll('img');
+//     images.forEach(function(img){
+//         img.style.filter =  "blur(" + filters.blur  + "px)";
+//     })
 // }
-// topPadding();
+
+// function changeSize(){
+//     filters.size = size.value;
+//     let images = imgContainer.querySelectorAll('img');
+//     images.forEach(function(img){
+//         img.style.height = filters.size + "px";
+//     })
+// }
+
+// function changeContrast(){
+//     filters.contrast = contrast.value;
+//     let images = imgContainer.querySelectorAll('img');
+//     images.forEach(function(img){
+//         img.style.filter = "contrast(" + filters.contrast + "%)";
+//     })
+// }
+
+// function changeInvert(){
+//     filters.invert = invert.value;
+//     let images = imgContainer.querySelectorAll('img');
+//     images.forEach(function(img){
+//         img.style.filter = "invert(" + filters.invert + "%)";
+//     })
+// }
+
+// function changeSaturation(){
+//     filters.saturation = saturation.value;
+//     let images = imgContainer.querySelectorAll('img');
+//     images.forEach(function(img){
+//         img.style.filter = "saturate(" + filters.saturation + "%)";
+//     })
+// }
+
+
+
+
+
+    /*    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error status: ${response.status}`);
+        }
+        return response.json();
+    })*/
